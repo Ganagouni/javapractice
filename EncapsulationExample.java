8a6c770fca34c15cc47caa85b11b80b15937403a
ganagouni <ganagouni.sai@gmail.com>
2018-08-05 15:10:23 +0200

    corrections

--- EncapsulationExample.java
+++ EncapsulationExample.java
@@ -1,6 +1,6 @@
 class Employees{
 
-private int numOfEmployess=0;
+private int NumOfEmployees=0;
 
 public void setNumOfEmployees(int count)
 {
@@ -8,14 +8,14 @@ public void setNumOfEmployees(int count)
 }
 public double getNumOfEmployees()
 {
-	return numOfEmployees;
+	return NumOfEmployees;
 }
 }
 public class EncapsulationExample{
 public static void main (String [] args)
 {
 	Employees emp= new Employees();
-	emp.setNumOfEMployees(12345);
-	System.out.println("Number of Employees are: "emp.getNumOfEmployees());
+	emp.setNumOfEmployees(12345);
+	System.out.println("Number of Employees are: "+emp.getNumOfEmployees());
 }
 }
